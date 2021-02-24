a51ed6d53c6e986ff0e96fa2554eefe28830e512
pushkine <pushkine.github@gmail.com>
2021-02-24 05:15:04 +0100

    works

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
-import { override_languageServiceHost } from "./transform/languageServiceHost";
-import { override_typescript } from "./transform/typescript";
-import { is_svelte_file } from "./transform/utils";
-export type ts = Parameters<ts.server.PluginModuleFactory>[0]["typescript"];
-interface SvelteTsPluginOptions {
-	name: string;
-}
-
-module.exports = function init(modules) {
-	const ts = modules.typescript;
-
-	override_typescript(ts);
-
-	return {
-		create(info: ts.server.PluginCreateInfo & { config: SvelteTsPluginOptions }): ts.LanguageService {
-			const { languageService, languageServiceHost } = info;
-			override_languageServiceHost(ts, languageServiceHost);
-			return languageService;
-		},
-		getExternalFiles(project: ts.server.ConfiguredProject) {
-			return project.getFileNames().filter(is_svelte_file);
-		},
-	};
-} as ts.server.PluginModuleFactory;
+import svelte2tsx, { SvelteCompiledToTsx } from "svelte2tsx";
+import { createLanguagePlugin } from "./ts-language-plugin";
+module.exports = createLanguagePlugin({
+	extension: "svelte",
+	transform(fileName, text) {
+		const tsx: SvelteCompiledToTsx = svelte2tsx(text, { filename: fileName, emitOnTemplateError: true });
+		return {
+			content: tsx.code,
+			mappings: tsx.map.mappings,
+		};
+	},
+});
